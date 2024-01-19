25aa318d100ec279077f7e3f83984e35778bb69a
Arul-1911 <arulpraveen19112002@gmail.com>
2024-01-19 21:39:20 +0530

    changes in flight data

--- scripts/flightData.js
+++ scripts/flightData.js
@@ -5,23 +5,60 @@ const Flight = require('../models/flight'); //getting schema
 const flightData = {
     flights:[
         {
-        "departure": "chennai",
+        "departure": "madurai",
       "arrival": "coimbatore",
       "departure_time": "08:00 AM",
-      "arrival_time": "09:30 AM",
+      "arrival_time": "9:30 AM",
       "class": "Business",
       "price": 500.00,
       "plane_name": "Airbus A320"
     },
     {
-      "departure": "chennai",
+        "departure": "madurai",
+      "arrival": "coimbatore",
+      "departure_time": "10:00 AM",
+      "arrival_time": "11:30 AM",
+      "class": "Economy",
+      "price": 200.00,
+      "plane_name": "Airbus A320"
+    },
+    {
+        "departure": "madurai",
+        "arrival": "coimbatore",
+        "departure_time": "12:00 PM",
+        "arrival_time": "01:30 PM",
+        "class": "Business",
+        "price": 600.00,
+        "plane_name": "Boeing B737"   
+      },
+    
+    {
+      "departure": "madurai",
+      "arrival": "coimbatore",
+      "departure_time": "10:00 PM",
+      "arrival_time": "12:30 AM",
+      "class": "Economy",
+      "price": 300.00,
+      "plane_name": "Boeing B737"   
+    },
+    {
+      "departure": "madurai",
       "arrival": "coimbatore",
       "departure_time": "12:00 PM",
       "arrival_time": "01:30 PM",
       "class": "Economy",
-      "price": 300.00,
-      "plane_name": "Boeing 737"   
-    }
+      "price": 200.00,
+      "plane_name": "Indigo B737"   
+    },
+    {
+        "departure": "madurai",
+        "arrival": "coimbatore",
+        "departure_time": "09:00 PM",
+        "arrival_time": "11:30 PM",
+        "class": "Business",
+        "price": 700.00,
+        "plane_name": "Indigo B737"   
+      }
 ]
 };
 
